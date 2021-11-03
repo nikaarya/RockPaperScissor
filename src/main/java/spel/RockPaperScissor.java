748b153bf91ee4537e8fa786bc9642e03f3e0431
nikaarya <nik.ary94@gmail.com>
2021-11-03 09:38:59 +0100

    Added all game files

---
+++ src/main/java/spel/RockPaperScissor.java
@@ -0,0 +1,79 @@
+package spel;
+
+import java.util.Scanner;
+
+public class RockPaperScissor {
+    static Scanner sc = new Scanner(System.in);
+
+    static Boolean exit = false;
+    static String[] weapons = {"Rock", "Paper", "Scissor"};
+    static String compWon = "Computer won";
+    static String playWon = "Player won";
+    static String compChoice = "Computers choice: ";
+
+    public static void main(String[] args) {
+        playOrNot();
+    }
+
+    private static void playOrNot() {
+        System.out.println("Press 1 to play rock, paper, scissor, press 0 to exit");
+        int playOrExit = sc.nextInt();
+        sc.nextLine();
+        if (playOrExit == 1) {
+            game();
+        } else if (playOrExit == 0) {
+            exit = true;
+            System.out.println("Terminating....");
+        } else {
+            System.out.println("Invalid option, try again");
+            playOrNot();
+        }
+    }
+    public static void game() {
+        while (!exit) {
+            int computerRandom = (int) (Math.random() * 3);
+            String computersChoice = weapons[computerRandom];
+
+            System.out.println("Rock, paper or scissor? Press 0 to exit");
+            String rockPaperScissor = sc.next();
+            if (rockPaperScissor.equalsIgnoreCase("0")) {
+                exit = true;
+                System.out.println("Terminating....");
+            }
+            else if (rockPaperScissor.equalsIgnoreCase(computersChoice)) {
+                System.out.println(compChoice + computersChoice);
+                System.out.println("Draw");
+            }
+            else if (rockPaperScissor.equalsIgnoreCase("rock") && computersChoice.equals("Paper")) {
+                ifComputerWins(computersChoice);
+            }
+            else if (rockPaperScissor.equalsIgnoreCase("rock") && computersChoice.equals("Scissor")) {
+                ifPlayerWins(computersChoice);
+            }
+            else if (rockPaperScissor.equalsIgnoreCase("paper") && computersChoice.equals("Rock")) {
+                ifPlayerWins(computersChoice);
+            }
+            else if (rockPaperScissor.equalsIgnoreCase("paper") && computersChoice.equals("Scissor")) {
+                ifComputerWins(computersChoice);
+            }
+            else if (rockPaperScissor.equalsIgnoreCase("scissor") && computersChoice.equals("Rock")) {
+                ifComputerWins(computersChoice);
+            }
+            else if (rockPaperScissor.equalsIgnoreCase("scissor") && computersChoice.equals("Paper")) {
+                ifPlayerWins(computersChoice);
+            }
+            else {
+                System.out.println("Wrong input, try again");
+                game();
+            }
+        }
+    }
+    private static void ifComputerWins(String computersChoice) {
+        System.out.println(compChoice + computersChoice);
+        System.out.println(compWon);
+    }
+    private static void ifPlayerWins(String computersChoice) {
+        System.out.println(compChoice + computersChoice);
+        System.out.println(playWon);
+    }
+}
---
+++ src/test/java/RockPaperScissorTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import spel.RockPaperScissor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RockPaperScissorTest {
+
+    RockPaperScissor rockPaperScissor = new RockPaperScissor();
+
+    @Test
+    void givenScissorAndComputerGivesRockComputerShouldWin() {
+        //assertEquals("Scissor", rockPaperScissor.);
+
+    }
+
+}
